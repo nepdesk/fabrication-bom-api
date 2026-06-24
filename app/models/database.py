9f46d52ad553325e5cd72d6c5e2b811205c50dfc
Nepdesk <nepdesk@gmail.com>
2026-06-24 16:45:28 +0530

    fix: remove unused import os in database.py

--- app/models/database.py
+++ app/models/database.py
@@ -13,7 +13,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-import os
 import sqlite3
 from pathlib import Path
 from typing import Any, Dict, List, Tuple
